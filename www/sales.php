9fbbfd540ebf0c225ac76c05c52334ced5292f0f
Gleb Gorokhov <gg@verstaburo.com>
2017-01-10 22:55:07 +0400

    Финальные правки по sales.php

--- www/sales.php
+++ www/sales.php
@@ -38,46 +38,40 @@
         <!-- // Переключатели контента -->
         
         <!-- Список акций -->
-        <ul class="salespage-actionlist">
+        <ul class="action-blocks-list">
         	<li class="action-block">
         		<h3 class="action-block__heading">Оплата</h3>
-        		<p>Дорогие друзья! Вы можете оплатить услуги нашей клиники, любым удобным для Вас способом!</p>
+        		<p class="action-block__text">Дорогие друзья! Вы можете оплатить услуги нашей клиники, любым удобным для Вас способом!</p>
                 <p class="action-block__bottominfo"><a href="#">Перейти</a></p>
                 <img src="#" alt="@@" class="action-block__image" width="171" height="158"/>
         	</li>
         	<li class="action-block action-block_lightblue">
         		<h3 class="action-block__heading">Новость</h3>
-        		<p>Лучший подарок для мужа на Новый<br>
-        		год это конечно же большая грудь.<br>
-        		Узнайте сейчас о всех предложениях.</p>
+        		<p class="action-block__text">Лучший подарок для мужа на Новый год это конечно же большая грудь. Узнайте сейчас о всех предложениях.</p>
         		<p class="action-block__bottominfo">1 декабря — 1 февраля</p>
                 <img src="" class="action-block__image action-block__image_bg" width="275"/>
         	</li>
         	<li class="action-block action-block_lightgold">
         		<h3 class="action-block__heading">Акция</h3>
-        		<p>Омолодить тело, подтянуть овал лица или скорректировать недостатки? Справиться с этими проблемами поможет аппарат «Титан». <br>
-        		Весь октябрь скидки 15%!</p>
+        		<p class="action-block__text">Омолодить тело, подтянуть овал лица или скорректировать недостатки? Справиться с этими проблемами поможет аппарат «Титан». Весь октябрь скидки 15%!</p>
         		<p class="action-block__bottominfo">1 октября — 29 октября</p>
                 <img src="" class="action-block__image action-block__image_bg" width="245"/>
         	</li>
         	<li class="action-block">
         		<h3 class="action-block__heading">Оплата</h3>
-        		<p>Дорогие друзья! Вы можете оплатить услуги нашей клиники, любым удобным для Вас способом!</p>
+        		<p class="action-block__text">Дорогие друзья! Вы можете оплатить услуги нашей клиники, любым удобным для Вас способом!</p>
         		<p class="action-block__bottominfo"><a href="#">Перейти</a></p>
                 <img src="" class="action-block__image" width="171" height="158"/>
         	</li>
         	<li class="action-block action-block_lightblue">
         		<h3 class="action-block__heading">Новость</h3>
-        		<p>Лучший подарок для мужа на Новый<br>
-        		год это конечно же большая грудь.<br>
-        		Узнайте сейчас о всех предложениях.</p>
+        		<p class="action-block__text">Лучший подарок для мужа на Новый год это конечно же большая грудь. Узнайте сейчас о всех предложениях.</p>
         		<p class="action-block__bottominfo">1 декабря — 1 февраля</p>
                 <img src="" class="action-block__image action-block__image_bg"  width="275"/>
         	</li>
         	<li class="action-block action-block_lightgold">
         		<h3 class="action-block__heading">Акция</h3>
-        		<p>Омолодить тело, подтянуть овал лица или скорректировать недостатки? Справиться с этими проблемами поможет аппарат «Титан». <br>
-        		Весь октябрь скидки 15%!</p>
+        		<p class="action-block__text">Омолодить тело, подтянуть овал лица или скорректировать недостатки? Справиться с этими проблемами поможет аппарат «Титан». Весь октябрь скидки 15%!</p>
         		<p class="action-block__bottominfo">1 октября — 29 октября</p>
                 <img src="" class="action-block__image action-block__image_bg" width="245"/>
         	</li>

23e2a61ae958e14e42485d53855d8a7f55b486ac
Ilya <rewasd91@mail.ru>
2017-01-10 18:36:43 +0300

    Еще 3 странички (html)

--- www/contacts.php
+++ www/contacts.php
@@ -35,7 +35,50 @@
         </div>  
         <!-- // Блок с картой --> 
     	
-    	
+    	<!-- Верхний контент -->
+        <div class="columns page-content-padding">
+            <div class="columns__third">
+                <p class="smalltitle">Обязательно приходите к нам:</p>
+                <p class="columns__maintext">
+                	<span class="contact">Пироговская набережная., д. 5/2.</span><br><br>
+                	Для Вашего удобства для гостей клиники напротив входа в отель «Санкт-Петербург» расположена бесплатная парковка.<br><br>
+                	Так же у нас работает многоканальный номер телефона:<br><br>
+                	<span class="contact">Пироговская набережная., д. 5/2.</span><br><br>
+                	Либо же напишите нам письмо:<br><br>
+                	<span class="contact"><a href="#">hi@spika.clinic</a></span>
+                </p>
+            </div>
+            <form class="columns__two-of-three" action="send.php" method="post">
+                <p class="smalltitle">Напишите нам прямо сейчас:</p>
+                <input type="text" name="name" placeholder="Как вас зовут?">
+                <input type="email" name="email">
+                <p>или номер телефона</p>
+                <input type="tel" name="tel">
+                <textarea name="message" placeholder="Ваше сообщение"></textarea>
+                <button class="button">Отправить</button>
+            </form>
+        </div>
+        <!-- // Верхний контент -->
+        
+        <!-- Нижний контент -->
+        <div class="columns page-content-padding">
+            <div class="columns__one">
+                <p class="smalltitle">Как еще к нам можно проехать:</p>
+                <div class="path">
+                	<p>Проезд со стороны Литейного проезда</p>
+                	<img src="" alt="">
+                </div>
+                <div class="path">
+                	<p>Проезд со стороны м. «Горьковская»</p>
+                	<img src="" alt="">
+                </div>
+                <div class="path">
+                	<p>Проезд со стороны м. «Чёрная речка»</p>
+                	<img src="" alt="">
+                </div>
+            </div>
+        </div>
+        <!-- // Нижний контент -->
         
     </div>
 </main>
---
+++ www/patients.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+    <?php include "blocks/head.php"; ?>
+    <meta name="robots" content="all">
+    <?php include "blocks/meta.php"; ?>
+    <title></title>
+    <?php include "blocks/css.php"; ?>
+</head>
+
+<body> 
+
+<?php include "blocks/header.php"; ?>
+
+<!-- Заголовок + хлебные крошки -->
+<section class="page-heading">
+    <div class="container">
+        <div class="page-heading__container">
+            <ul class="page-heading__breadcrumbs">
+                <li><a href="#">Главная страница</a></li>
+            </ul>
+            <h1 class="page-heading__heading">ПАЦИЕНТАМ</h1>
+        </div>
+    </div>
+</section>
+<!-- // Заголовок + хлебные крошки -->
+
+<!-- Контент страницы  -->
+<main class="main-content patients">
+    <div class="container">
+    	
+    	<!-- Блок с картинкой -->
+        <div class="image-block">
+            <img src="" alt="">
+        </div>  
+        <!-- // Блок с картинкой --> 
+    	
+    	<!-- Переключатели контента -->
+        <ul class="content-changer">
+            <li class="active">общая информация</li>
+            <li>приём</li>
+            <li>палаты стационара</li>
+            <li>обследование перед операцией</li>
+            <li>операция</li>
+            <li>послеоперационный период</li>
+            <li>подарочный сертификат</li>
+        </ul>
+        <!-- // Переключатели контента -->
+        
+        <!-- 1 колонка -->
+        <div class="columns page-content-padding">
+            <div class="columns__one">
+                <p class="columns__maintext">При обращении в клинику «СПИКА» для получения интересующей вас информации или консультации, <br>
+                вы можете позвонить по телефонам 8 (812) 403-02-01.</p>
+                <p class="columns__maintext">Администраторы клиники любезно ответят вам на вопросы об оказываемых услугах, их стоимости, специалистах,<br>
+                графике работы клиники и многом другом.</p>
+                <p class="columns__maintext">Более подробную информацию вы сможете получить непосредственно на консультации специалиста.</p>
+                <p class="columns__maintext">Административно-диспетчерская служба клиники «СПИКА» работает ежедневно с 9:00 до 21:00.<br>
+                Просим вас отнестись с пониманием.<br>
+                Для получения полной информации о стоимости и особенностях процедур и услуг клиники рекомендуем записаться<br>
+                на консультацию к соответствующему специалисту в удобное для вас время.</p>
+            </div>
+        </div>
+        <!-- // 1 колонка -->
+        
+    </div>
+</main>
+<!-- // Контент страницы  -->
+
+<?php include "blocks/footer.php"; ?>
+
+<?php include "blocks/forms.php"; ?>
+<?php include "blocks/scripts.php"; ?>
+</body>
+</html>

d6331b5b1770bc2c0a377cfccadd7e288de12a2c
Ilya <rewasd91@mail.ru>
2017-01-10 20:13:43 +0300

    CSS sales.php

--- www/sales.php
+++ www/sales.php
@@ -38,20 +38,22 @@
         <!-- // Переключатели контента -->
         
         <!-- Список акций -->
-        <ul class="salespage-actionlist"> <!-- display:flex; flex-wrap:wrap; justify-content:space-between. Стиль этого блока пишешь в default.scss -->
-        	<li class="action-block"> <!-- .action-block - добавляешь в basics.scss. Т.е. весь стиль блока и вариации описываешь там. Блоку этому надо еще прописать min-height как в макете -->
+        <ul class="salespage-actionlist">
+        	<li class="action-block">
         		<h3 class="action-block__heading">Оплата</h3>
-        		<p>Дорогие друзья! Вы можете оплатить услуги нашей клиники, любым удобным для Вас способом!</p> <!-- У этой штуки будет flex:1, чтобы она растягивалась от заголовка до нижнего текста. Чтобы нижний текст выравнивался по низу блока. -->
+        		<p>Дорогие друзья! Вы можете оплатить услуги нашей клиники, любым удобным для Вас способом!</p>
                 <p class="action-block__bottominfo"><a href="#">Перейти</a></p>
-                <img src="" class="action-block__image"/> <!-- Картинка справа. position:absolute. У нее будет отступ справа. А также max-height:80% и max-width: такая-максимальная-ширина-чтобы-на-текст-не-налезало -->
+                <img src="#" alt="@@" class="action-block__image"/>
         	</li>
-        	<li class="action-block action-block_lightblue"> <!-- action-block_lightblue - Класс для голубого фона. Темный цвет заголовка прописываешь внутри этого класса. -->
+        	<li class="action-block action-block_lightblue">
         		<h3 class="action-block__heading">Новость</h3>
-        		<p>Лучший подарок для мужа на Новый год это конечно же большая грудь. Узнайте сейчас о всех предложениях.</p>
+        		<p>Лучший подарок для мужа на Новый<br>
+        		год это конечно же большая грудь.<br>
+        		Узнайте сейчас о всех предложениях.</p>
         		<p class="action-block__bottominfo">1 декабря — 1 февраля</p>
-                <img src="" class="action-block__image action-block__image_bg"/> <!-- Это класс, который делает картинке height:100%; max-height:100%; max-width:initial; top:0; right:0; Т.е. делает картинку полностью заполняющей блок от верха до низа -->
+                <img src="" class="action-block__image action-block__image_bg"/>
         	</li>
-        	<li class="action-block"> <!-- action-block_lightblue - Класс для светло-золотого фона. Темный цвет заголовка прописываешь внутри этого класса. -->
+        	<li class="action-block action-block_lightgold">
         		<h3 class="action-block__heading">Акция</h3>
         		<p>Омолодить тело, подтянуть овал лица или скорректировать недостатки? Справиться с этими проблемами поможет аппарат «Титан». <br>
         		Весь октябрь скидки 15%!</p>
@@ -66,7 +68,9 @@
         	</li>
         	<li class="action-block">
         		<h3 class="action-block__heading">Новость</h3>
-        		<p>Лучший подарок для мужа на Новый год это конечно же большая грудь. Узнайте сейчас о всех предложениях.</p>
+        		<p>Лучший подарок для мужа на Новый<br>
+        		год это конечно же большая грудь.<br>
+        		Узнайте сейчас о всех предложениях.</p>
         		<p class="action-block__bottominfo">1 декабря — 1 февраля</p>
                 <img src="" class="action-block__image action-block__image_bg"/>
         	</li>
